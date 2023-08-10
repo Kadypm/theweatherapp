3e61bd5a4c092bbda3483373c8211ace3f2f83d4
FWebMom <fkperezm@gmail.com>
2023-08-10 21:55:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 function formatDate(date) {
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-  
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    let dayIndex = date.getDay();
-  
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let days = days[dayIndex];
-    return `${days[dayIndex]} ${hours}:${minutes}`; 
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
-  
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let h2 = days[dayIndex];
+  return `${days[dayIndex]} ${hours}:${minutes}`; 
+}
+
+
   function search(event) {
     event.preventDefault();
     let h1 = document.querySelector("h1");
@@ -49,3 +51,5 @@ function formatDate(date) {
   
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", search);
+  
+  //AXIOS current temp
